6bd6d5155a907d4e0802767ee60e2a0cda7b4e19
Marcelo Ierardi <164015400+MarceloLierardi@users.noreply.github.com>
2026-06-01 02:54:04 -0300

    Delete scripts directory

--- scripts/canvas_layer.gd
+++
@@ -1,9 +0,0 @@
-extends CanvasLayer
-
-@onready var label_pontuacao: Label = $Label
-
-func _process(delta: float) -> void:
-	atualizar_label()
-
-func atualizar_label():
-	label_pontuacao.text = "Coins: " + str(Global.total_coins)
